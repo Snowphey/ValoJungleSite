b09d2da760dd376ba39c7af463f26a47d2b91c2e
Snowphey <slongy.pro@gmail.com>
2023-10-19 13:59:31 +0200

    Création et modfication de nouveaux utilisateurs par les admins

--- src/main/java/fr/cytech/pau/ValoJungleSite/entity/Utilisateur.java
+++ src/main/java/fr/cytech/pau/ValoJungleSite/entity/Utilisateur.java
@@ -7,16 +7,44 @@ import java.time.LocalDateTime;
 @Entity
 public class Utilisateur {
     @Id
-    @Getter
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Getter
     private String username;
 
-    @Getter
     private String password;
 
-    @Getter
     private String role;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
